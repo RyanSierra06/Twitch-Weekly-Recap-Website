48c6e76abee093226333b91b596925bc160773ff
RyanSierra06 <rsierra@purdue.edu>
2025-07-23 00:37:48 -0700

    update backend

--- backend/config/session.js
+++ backend/config/session.js
@@ -1,15 +1,9 @@
 import session from 'express-session';
 
 const SESSION_SECRET = process.env.SESSION_SECRET;
-const FRONTEND_BASE_URL = process.env.FRONTEND_BASE_URL;
 
 export default session({
   secret: SESSION_SECRET,
   resave: false,
-  saveUninitialized: false,
-  cookie: {
-    secure: true,
-    sameSite: 'None',
-    domain: FRONTEND_BASE_URL,
-  }
+  saveUninitialized: false
 });
