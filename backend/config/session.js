eba9eb772c099fdccb3d94a20a7dda6efa269795
RyanSierra06 <rsierra@purdue.edu>
2025-07-23 00:34:21 -0700

    update backend for production

--- backend/config/session.js
+++ backend/config/session.js
@@ -1,14 +1,15 @@
 import session from 'express-session';
 
 const SESSION_SECRET = process.env.SESSION_SECRET;
+const FRONTEND_BASE_URL = process.env.FRONTEND_BASE_URL;
 
 export default session({
   secret: SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
   cookie: {
-    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-    secure: process.env.NODE_ENV === 'production', // Must be true for HTTPS (Render)
-    maxAge: 24 * 60 * 60 * 1000 // 1 day
+    secure: true,
+    sameSite: 'None',
+    domain: FRONTEND_BASE_URL,
   }
 });
